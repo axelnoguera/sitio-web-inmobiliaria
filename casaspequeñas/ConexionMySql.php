6dbee151c9df32d2aea5744f16318d1210f25f50
axelnoguera <axelprogramacion123@gmail.com>
2025-05-20 16:33:03 -0300

    Update ConexionMySql.php

--- casaspequeñas/ConexionMySql.php
+++ casaspequeñas/ConexionMySql.php
@@ -1,1 +1,9 @@
+<?php
+$host = "localhost";
+$dbnombre = "database inmobiliaria";
+$usuario = "root";
+$contraseña = "";
 
+$conexion = new PDO("mysql:host=$host;dbname=$dbnombe;charset=utf8mb4",$usuario,$contraseña);
+
+?>
